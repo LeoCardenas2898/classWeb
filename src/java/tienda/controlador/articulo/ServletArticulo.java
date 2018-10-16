13919b76ac94de9413b7ae38fb207d7df6dd0b59
Leonardo Cárdenas <leo@admin.com>
2018-10-15 20:12:04 -0500

    webApp3

--- src/java/tienda/controlador/articulo/ServletArticulo.java
+++ src/java/tienda/controlador/articulo/ServletArticulo.java
@@ -13,40 +13,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Alumno
- */
-@WebServlet(name = "ServletArticulo", urlPatterns = {"/ServletArticulo"})
+
+@WebServlet(name = "ServletArticulo", urlPatterns = {"/ServletArticulo","/verArticulo"})
 public class ServletArticulo extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletArticulo</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletArticulo at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
-            out.close();
-        }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
